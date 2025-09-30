9baf83623fd52ac75e743738a6e9f5bfac9e5f6e
stockcorp <97267976+stockcorp@users.noreply.github.com>
2025-10-01 05:48:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -333,4 +333,5 @@ History.add_to_db = add_to_db
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render 會自動提供 PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
